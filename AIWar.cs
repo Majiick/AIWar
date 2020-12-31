41a58551d163ff27972edc4892295492fbc3f5a2
Zan Smirnov <c15437072@mydit.ie>
2020-12-31 01:46:15 +0000

    Clean up

--- AIWar.cs
+++ AIWar.cs
@@ -135,15 +135,15 @@ namespace AI_War
 		public class PlaceBombEvent : Event {
 			public Bomb bomb;
 		}
-		private DynValue PlaceBomb(Player player) {
-			Table ret = new Table(player.dynamicScript);
+		private DynValue PlaceBomb(string playerName, Script ownerScript) {
+			Table ret = new Table(ownerScript);
 			ret.Set(1, DynValue.Nil);
 			ret.Set(2, DynValue.NewNumber((int)API_RETURNS.ERROR));
 
 			Ship playerShip = null;
 			foreach (Cell cell in map.cells.Cast<Cell>()) {
 				foreach (IGameObject go in cell) {
-					if (go is Ship && ((Ship)go).owner == player.name) {
+					if (go is Ship && ((Ship)go).owner == playerName) {
 						playerShip = go as Ship;
 						break;
 					}
@@ -161,28 +161,28 @@ namespace AI_War
 			int id = IDCounter.NewID();
 			var bomb = new Bomb();
 			bomb.id = id;
-			bomb.owner = player.name;
+			bomb.owner = playerName;
 			bomb.x = playerShip.x;
 			bomb.y = playerShip.y;
 			moveAndPlaceBombEvents.Add(new PlaceBombEvent { bomb = bomb });
 
-			ret.Set(1, DynValue.NewTable(bomb.Table(player.dynamicScript)).CloneAsWritable());
+			ret.Set(1, DynValue.NewTable(bomb.Table(ownerScript)).CloneAsWritable());
 			ret.Set(2, DynValue.NewNumber((int)API_RETURNS.OK));
 			return DynValue.NewTable(ret);
 		}
-		public Func<DynValue> PlaceBombBind(Player p) {
-			return () => PlaceBomb(p);
+		public Func<DynValue> PlaceBombBind(string playerName, Script ownerScript) {
+			return () => PlaceBomb(playerName, ownerScript);
 		}
 
 		public class BombExplodeEvent {
 			public Bomb bomb;
 		}
 		public ConcurrentBag<BombExplodeEvent> bombExplodeEvents = new ConcurrentBag<BombExplodeEvent>();
-		private int ExplodeBomb(Player player, int bombID) {
+		private int ExplodeBomb(string playerName, int bombID) {
 			Ship playerShip = null;
 			foreach (Cell cell in map.cells.Cast<Cell>()) {
 				foreach (IGameObject go in cell) {
-					if (go is Ship && ((Ship)go).owner == player.name) {
+					if (go is Ship && ((Ship)go).owner == playerName) {
 						playerShip = go as Ship;
 						break;
 					}
@@ -203,25 +203,25 @@ namespace AI_War
 			if (bombToExplode == null) {
 				return (int)API_RETURNS.NOT_FOUND;
 			}
-			if (bombToExplode.owner != player.name) {
+			if (bombToExplode.owner != playerName) {
 				return (int)API_RETURNS.NO_PERMISSION;
 			}
 			bombExplodeEvents.Add(new BombExplodeEvent { bomb = bombToExplode });
 			return (int)API_RETURNS.OK;
 		}
 
-		public Func<int, int> ExplodeBomb(Player player) {
-			return (t2) => ExplodeBomb(player, t2);
+		public Func<int, int> ExplodeBomb(string playerName) {
+			return (t2) => ExplodeBomb(playerName, t2);
 		}
 
-		private DynValue MyShip(Player player) {
-			Table ret = new Table(player.dynamicScript);
+		private DynValue MyShip(string playerName, Script ownerScript) {
+			Table ret = new Table(ownerScript);
 			ret.Set(1, DynValue.Nil);
 			ret.Set(2, DynValue.NewNumber((int)API_RETURNS.NOT_FOUND));
 			foreach (Cell cell in map.cells.Cast<Cell>()) {
 				foreach (IGameObject go in cell) {
-					if (go is Ship && ((Ship)go).owner == player.name) {
-						ret.Set(1, DynValue.NewTable(go.Table(player.dynamicScript)));
+					if (go is Ship && ((Ship)go).owner == playerName) {
+						ret.Set(1, DynValue.NewTable(go.Table(ownerScript)));
 						ret.Set(2, DynValue.NewNumber((int)API_RETURNS.OK));
 						break;
 					}
@@ -231,8 +231,8 @@ namespace AI_War
 			return DynValue.NewTable(ret);
 		}
 
-		public Func<DynValue> MyShipBind(Player player) {
-			return () => MyShip(player);
+		public Func<DynValue> MyShipBind(string playerName, Script ownerScript) {
+			return () => MyShip(playerName, ownerScript);
 		}
 
 		public class MoveEvent : Event {
@@ -282,8 +282,8 @@ namespace AI_War
 			public int x;
 			public int y;
 		}
-		private DynValue CreateShip(Player player, int x, int y) {
-			Table ret = new Table(player.dynamicScript);
+		private DynValue CreateShip(string playerName, Script ownerScript, int x, int y) {
+			Table ret = new Table(ownerScript);
 			ret.Set(1, DynValue.Nil);
 			ret.Set(2, DynValue.NewNumber((int)API_RETURNS.NOT_FOUND));
 			if (x > Map.WIDTH || y > Map.HEIGHT || x <= 0 || y <= 0) {
@@ -292,26 +292,26 @@ namespace AI_War
 			}
 			foreach(Cell cell in map.cells.Cast<Cell>()) {  // Check if ship already exists on map.
 				foreach(IGameObject go in cell) {
-					if (go is Ship && ((Ship)go).owner == player.name) {
+					if (go is Ship && ((Ship)go).owner == playerName) {
 						ret.Set(2, DynValue.NewNumber((int)API_RETURNS.ERROR));
 						return DynValue.NewTable(ret);
 					}
 				}
 			}
 
-			if (addShipBag.Any(x => x.owner == player.name)) {  // Check if already tried to create ship this tick.
+			if (addShipBag.Any(x => x.owner == playerName)) {  // Check if already tried to create ship this tick.
 				ret.Set(2, DynValue.NewNumber((int)API_RETURNS.ERROR));
 				return DynValue.NewTable(ret);
 			}
 
 			int id = IDCounter.NewID();
-			addShipBag.Add(new AddShipEvent { id = id, owner = player.name, x = x, y = y });
+			addShipBag.Add(new AddShipEvent { id = id, owner = playerName, x = x, y = y });
 			ret.Set(1, DynValue.NewNumber(id));
 			ret.Set(2, DynValue.NewNumber((int)API_RETURNS.OK));
 			return DynValue.NewTable(ret);
 		}
-		public Func<int, int, DynValue> CreateShip(Player player) {
-			return (t2, t3) => CreateShip(player, t2, t3);
+		public Func<int, int, DynValue> CreateShip(string playerName, Script owner) {
+			return (t2, t3) => CreateShip(playerName, owner, t2, t3);
 		}
 	}
 
@@ -319,8 +319,6 @@ namespace AI_War
 		public string name;
 		public string script;
 		public Memory memory;
-
-		public Script dynamicScript;  // Reference to the current tick's script.
 	}
 
 	static class IDCounter {
@@ -361,22 +359,22 @@ namespace AI_War
 		}
 
 		public static Script InitializeScript(Player p) {
-			p.dynamicScript = new Script(CoreModules.Preset_SoftSandbox);
-			var s = p.dynamicScript;
-			((ScriptLoaderBase)s.Options.ScriptLoader).ModulePaths = ScriptLoaderBase.UnpackStringPaths("C:\\Users\\Ecoste\\Desktop\\AIWar\\AIWar\\?.lua");
-			s.Globals["move"] = api.Move(p.name);
-			s.Globals["create_ship"] = api.CreateShip(p);
-			s.Globals["map"] = map.LuaMap(s);
-			s.Globals["my_ship"] = api.MyShipBind(p);
-			s.Globals["place_bomb"] = api.PlaceBombBind(p);
-			s.Globals["incrementing_number"] = (Func<int>)(() => { return RandomSeed.NewNumber(); });
-			s.Globals["explode"] = api.ExplodeBomb(p);
+			Script script = new Script(CoreModules.Preset_SoftSandbox);
+
+			((ScriptLoaderBase)script.Options.ScriptLoader).ModulePaths = ScriptLoaderBase.UnpackStringPaths("C:\\Users\\Ecoste\\Desktop\\AIWar\\AIWar\\?.lua");
+			script.Globals["move"] = api.Move(p.name);
+			script.Globals["create_ship"] = api.CreateShip(p.name, script);
+			script.Globals["map"] = map.LuaMap(script);
+			script.Globals["my_ship"] = api.MyShipBind(p.name, script);
+			script.Globals["place_bomb"] = api.PlaceBombBind(p.name, script);
+			script.Globals["incrementing_number"] = (Func<int>)(() => { return RandomSeed.NewNumber(); });
+			script.Globals["explode"] = api.ExplodeBomb(p.name);
 			if (!String.IsNullOrEmpty(p.memory.json)) {
 				var t = JsonTableConverter.JsonToTable(p.memory.json);
-				s.Globals["memory"] = JsonTableConverter.JsonToTable(p.memory.json, s);
+				script.Globals["memory"] = JsonTableConverter.JsonToTable(p.memory.json, script);
 			}
-			s.DoFile("C:\\Users\\Ecoste\\Desktop\\AIWar\\AIWar\\player_base.lua");
-			return s;
+			script.DoFile("C:\\Users\\Ecoste\\Desktop\\AIWar\\AIWar\\player_base.lua");
+			return script;
 		}
 
 		struct ScriptRunResult {
@@ -561,7 +559,7 @@ namespace AI_War
 		static void DebugAddScript(ref List<Player> players, string user, string script) {
 			players.Add(new Player { script = script, name = user, memory = new Memory() });
 		}
-		class oneOffScript {
+		public class oneOffScript {
 			public string script;
 			public bool executed;
 		}
